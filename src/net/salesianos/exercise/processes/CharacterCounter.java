d14e148bcc156d6e6865f6f016bfb8437c4dcac9
ravel01106 <ravello2002@gmail.com>
2023-10-04 14:04:48 +0100

    feat: the CharacterCounter class is working correctly

--- src/net/salesianos/exercise/processes/CharacterCounter.java
+++ src/net/salesianos/exercise/processes/CharacterCounter.java
@@ -1,5 +1,35 @@
 package net.salesianos.exercise.processes;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CharacterCounter {
-    
+    public static void main(String[] args) {
+        String character = args[0].toLowerCase();
+        String route = "./src/net/salesianos/exercise/input/prueba.txt";
+        try {
+            BufferedReader bf = new BufferedReader( new FileReader(route));
+            String line = bf.readLine();
+            int counter = 0;
+            while (line != null) {
+                String characterLine = "";
+                for (int i = 0; i < line.length(); i++) {
+                    characterLine = String.valueOf(line.charAt(i)).toLowerCase();
+                    if (characterLine.equals(character)){
+                        counter++;
+                    }
+                }
+                line = bf.readLine();
+            }
+            System.out.println(counter);
+            bf.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
